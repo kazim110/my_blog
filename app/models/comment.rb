dbad12a4165f3af05dbab39d1da296bd28f71e8c
Kazim110 <kazim.mohammadi110@gmail.com>
2024-01-17 10:35:08 +0430

    fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ApplicationRecord
   after_save :update_comments_counter
 
   private
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes
 
   private
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
