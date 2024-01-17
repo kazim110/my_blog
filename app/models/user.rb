15d1d6a07a817f8e94037489e03a8caa8bd53e39
Kazim110 <kazim.mohammadi110@gmail.com>
2024-01-17 17:09:28 +0430

    validating user and post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validate :name, presence: true
+  validate :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   private
 
   def recent_posts
