8fd729825c38c209d808a5de0055375fa0e5318e
Kazim110 <kazim.mohammadi110@gmail.com>
2024-01-30 21:55:39 +0430

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,5 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
-
-    return unless @post.nil?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
-    return unless @user.nil?
-      head :not_found
-    end
   end
 end
