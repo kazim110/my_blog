8ea0fb5d2fde35803060b2dd0abc4a50770d281d
Kazim110 <kazim.mohammadi110@gmail.com>
2024-01-30 04:44:32 +0430

    fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   after_save :update_posts_counter
 
-  validates :title, presence: true, length: {maximum: 250}
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,36 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject { User.new(name: 'kazim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Developer', posts_counter: 5)}
-    before { subject.save }
+  subject { User.new(name: 'kazim', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Developer', posts_counter: 5) }
+  before { subject.save }
 
-    it 'is valid with valid attributes' do
-        expect(subject).to be_valid
-    end
-    
-    it 'Name should be present' do
-        subject.name = nil
-        expect(subject).to_not be_valid
-    end
-    
-    it 'posts_counter should be integer and equal and greater than zero' do
-        subject.posts_counter = -11
-        expect(subject).to_not be_valid
-    end
-    
-    it 'is valid with a positive integer posts_counter' do
-        subject.posts_counter = 0
-        expect(subject).to be_valid
-    end
-    
-    it 'is not valid with a non-integer posts_counter' do
-        subject.posts_counter = 3.5
-        expect(subject).not_to be_valid
-    end
-    
-    it 'is not valid with a string posts_counter' do
-        subject.posts_counter = 'abc'
-        expect(subject).not_to be_valid
-    end
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be integer and equal and greater than zero' do
+    subject.posts_counter = -11
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with a positive integer posts_counter' do
+    subject.posts_counter = 0
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid with a non-integer posts_counter' do
+    subject.posts_counter = 3.5
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid with a string posts_counter' do
+    subject.posts_counter = 'abc'
+    expect(subject).not_to be_valid
+  end
 end
-    
