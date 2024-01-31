0672063ccbabbd17cbf2d2324929796af32d511d
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-01 03:42:19 +0430

    add view and shows data from database in that

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @user = User.find_by(id: params[:user_id])
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: params[:user_id])
   end
 end
