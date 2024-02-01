7cd109542cee831fac5079327522e5807910d079
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-01 21:48:59 +0430

    fix n+1 problem in posts and users controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,13 @@ class PostsController < ApplicationController
   layout 'application'
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:comments, :likes, commands: [:user])
     @user = User.find_by(id: params[:user_id])
   end
 
   def show
-    @post = Post.find_by(id: params[:id])
-    @user = User.find_by(id: params[:user_id])
+    @post = Post.includes(:comments, likes: [:user], comments: [:user]).find(params[:id])
+    @user = current_user
   end
 
   def new
