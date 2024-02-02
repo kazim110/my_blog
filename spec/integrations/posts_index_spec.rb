7e98e2bcccce5a92944f3eda759f9e288093acf2
Mohammad Kazim <kazim.mohammadi110@gmail.com>
2024-02-02 11:57:36 +0430

    Update posts_index_spec.rb

--- spec/integrations/posts_index_spec.rb
+++ spec/integrations/posts_index_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 RSpec.feature 'User Post Index Page', type: :feature do
   before(:each) do
+    # Create a user and associated posts with comments and likes for testing
     @user = User.create(name: 'Test User', photo: 'test_photo.jpg')
     @user.save(validate: false)
     @post1 = @user.posts.create(text: 'Post Text 1')
@@ -9,25 +10,37 @@ RSpec.feature 'User Post Index Page', type: :feature do
     @post1.save(validate: false)
     @post2.save(validate: false)
 
+    # Create comments for posts
     @comment1 = @post1.comments.create(text: 'Comment 1', user: @user)
     @comment2 = @post1.comments.create(text: 'Comment 2', user: @user)
     @comment3 = @post2.comments.create(text: 'Comment 3', user: @user)
 
+    # Create likes for posts
     @like1 = @post1.likes.create(user: @user)
     @like2 = @post2.likes.create(user: @user)
     visit user_posts_path(@user)
   end
 
-  scenario 'Display user profile information and posts' do
+  scenario 'Disply user name' do
     expect(page).to have_content(@user.name)
-    expect(page).to have_css("img[src*='test_photo.jpg']")
+  end
 
+  scenario 'Display user photo' do
+    expect(page).to have_css("img[src*='test_photo.jpg']")
+  end
+
+  scenario 'Display user posts text' do
     expect(page).to have_content('Post Text 1')
     expect(page).to have_content('Post Text 2')
+  end
 
+  scenario 'Display user posts comments' do
     expect(page).to have_content(@comment1.text)
     expect(page).to have_content('Comment 2')
     expect(page).to have_content('Comment 3')
+  end
+
+  scenario 'Display user posts likes' do
     expect(page).to have_content('Likes: 1', count: 2)
     expect(page).to have_button('Like', count: 2, disabled: false)
   end
